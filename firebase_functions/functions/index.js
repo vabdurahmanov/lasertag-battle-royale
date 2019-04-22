1f6595ebd32b19a2014222a09e7e35e44cbc470a
Christian Pomales <christianpomales@gmail.com>
2019-04-21 17:56:08 -0700

    Added functions to start a new game and to add a new player.

--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+// Starts a new game with a single player
+exports.initalizeGame = functions.https.onRequest((request, response) => {
+  const playerInfo = {
+    [request.body.userID]: {
+      laserGunID: Number(request.body.laserGunID),
+      vestID: Number(request.body.vestID),
+      health: 100
+    }
+  };
+
+  admin.firestore().collection("game").add(playerInfo)
+    .then((docRef) => {
+      return response.send(docRef.id);
+    })
+    .catch((err) => {
+      console.log(err);
+      return response.send(err);
+    });
+});
+
+// Adds a player to an existing game
+exports.addPlayer = functions.https.onRequest((request, response) => {
+  const playerInfo = {
+    [request.body.userID]: {
+      laserGunID: Number(request.body.laserGunID),
+      vestID: Number(request.body.vestID),
+      health: 100
+    }
+  };
+
+  admin.firestore().collection("game").doc(request.body.gameID).update(playerInfo)
+    .then(() => {
+      return response.send("Success");
+    })
+    .catch((err) => {
+      console.log(err);
+      return response.send(err);
+    });
+});
