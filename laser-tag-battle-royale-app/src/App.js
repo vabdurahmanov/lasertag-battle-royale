54a7ab94089a089ef4fe4a789f10cb1acc6f8b83
Jasmine Kwong <jckwong0703@gmail.com>
2019-04-24 23:42:53 -0700

    Initializing react.js repo

---
+++ laser-tag-battle-royale-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Main</h1>
+      </div>
+   );
+  }
+}
+
+export default App;
---
+++ laser-tag-battle-royale-app/src/lobby.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Lobby extends React.Component {
+  render() {
+    return <h1>Lobby</h1>
+  }
+}
+
+export default Lobby;
