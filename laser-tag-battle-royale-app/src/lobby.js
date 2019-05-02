4522e858229f975c27d5d77ab79bd251d157c43a
joepark1993 <jpark259@ucr.edu>
2019-05-02 11:57:19 -0700

    You can now adjust the radius

--- laser-tag-battle-royale-app/src/lobby.js
+++ laser-tag-battle-royale-app/src/lobby.js
@@ -4,6 +4,10 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import {Map, InfoWindow, GoogleApiWrapper,Polygon} from 'google-maps-react';
 import {withGoogleMap,GoogleMap, Marker, Circle} from "react-google-maps";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 /*global google*/
 
 var currLoc= {
@@ -18,7 +22,8 @@ class Lobby extends React.Component {
     this.state = {
       lat: 33.98,
       lng: -117.4,
-      zoom: 12
+      zoom: 12,
+        radius:1000
     }
   }
   mapClick = (event) =>{ 
@@ -27,7 +32,9 @@ class Lobby extends React.Component {
       lat:event.latLng.lat(),
       lng:event.latLng.lng()
     })
- 
+  };
+   handleChange = (event) => {
+    this.setState({ radius: event.target.value });
   };
   render() {
     const GoogleMapExample = withGoogleMap(props => (
@@ -35,7 +42,7 @@ class Lobby extends React.Component {
         defaultCenter = { { lat: this.state.lat, lng: this.state.lng } }
         defaultZoom = { 13 }
       ><Circle defaultCenter={{ lat: this.state.lat, lng: this.state.lng }}
-                radius ={1000}>
+                radius ={this.state.radius}>
                 </Circle>
       </GoogleMap>
    ));
@@ -48,6 +55,23 @@ class Lobby extends React.Component {
         >
         
         </GoogleMapExample>
+        <div>
+        <FormControl>
+                  <Select
+            value={this.state.age}
+            onChange={this.handleChange}
+          >
+        
+        <InputLabel htmlFor="demo-controlled-open-select">Radius</InputLabel>
+            <MenuItem value={250}>250</MenuItem>
+            <MenuItem value={500}>500</MenuItem>
+            <MenuItem value={750}>750</MenuItem>
+            <MenuItem value={1000}>1000</MenuItem>
+            <MenuItem value={1500}>2000</MenuItem>
+            <MenuItem value={1700}>3000</MenuItem>
+          </Select>
+        </FormControl>
+        </div>
     </div>
     );
   }
