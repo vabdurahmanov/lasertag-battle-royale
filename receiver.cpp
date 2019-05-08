b4ed92057b65143dda3c52e14404d95b938cada5
vgordienko <gordienko.victoria@gmail.com>
2019-05-08 00:19:58 -0700

    Integrate esp32 with backend

--- receiver.cpp
+++ receiver.cpp
@@ -1,15 +1,52 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
 #define RECV_PIN 22  // 22 on esp32, 9 on uno
+
+const char* ssid = ""; 
+const char* pass = "";
+
 int values = 0; 
 
 void setup() {
 Serial.begin(115200);
 pinMode(RECV_PIN, INPUT); //receiver = input
+WiFi.begin(ssid, pass); 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("WiFi connected"); 
 }
 
 void loop(){
-   values = digitalRead(buttonPin);
-   while (values != 0){
+   values = digitalRead(RECV_PIN);
+     if (values != 1){
      Serial.println("...Incoming...");  
-     Serial.println(values);  
-   }
+     Serial.println(values); 
+      if (WiFi.status() == WL_CONNECTED){
+          HTTPClient http; 
+
+          http.begin("https://us-central1-lasertag-battle-royale.cloudfunctions.net/decrementHealth"); 
+          http.addHeader("Content-Type", "application/x-www-form-urlencoded"); 
+          int httpResponseCode = http.POST("vestID=1"); 
+
+          if (httpResponseCode > 0){
+            String response = http.getString(); 
+            Serial.println(httpResponseCode); 
+            Serial.println(response); 
+            }
+           else{
+            Serial.println("Error on sending POST: "); 
+            Serial.println(httpResponseCode); 
+            } 
+            
+            http.end(); 
+          }
+          else{
+            Serial.println("Error in WiFi connection"); 
+            }
+      }
+   
 }
