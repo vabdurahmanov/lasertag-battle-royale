f12cb72ca61fde0d0b8a64cf000a2c6ef030f394
Jasmine Kwong <jckwong0703@gmail.com>
2019-05-30 12:33:21 -0700

    pushing 'changes'

--- laser-tag-battle-royale-app/src/src/createroom.js
+++ laser-tag-battle-royale-app/src/src/createroom.js
@@ -6,42 +6,107 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
 import {Map, InfoWindow, GoogleApiWrapper,Polygon} from 'google-maps-react';
 import {withGoogleMap,GoogleMap, Marker, Circle} from "react-google-maps";
 import WaitingRoom from './waitingroom';
-
+import { timingSafeEqual } from 'crypto';
 
 class CreateRoom extends React.Component {
-    state = {
+  constructor(props){
+    super(props);
+
+    this.state = {
         lobby_name:"",
         num_players: 1,
         lat: 33.98,
         lng: -117.4,
         zoom: 12,
-        radius:1000,
+        radius:100,
         hasClicked: false,
-        test: this.props.testvalue
-
+        roompromise: null,
+        roomid: ''
     }
-
-    handleChange = (event)=>{
-        this.setState({num_players: event.target.value });
+  }
+    handleChange = (event) => {
+        this.setState({num_players : event.target.value});
     }
   mapClick = (event) =>{
     this.setState({
       lat:event.latLng.lat(),
       lng:event.latLng.lng()
     })
-    console.log(this.props.location.state.username)  //This holds the passed in prop from LOBBY
   };
    handleRadiusChange = (event) => {
     this.setState({ radius: event.target.value });
   };
 
-    createLobby=(event)=>{
-      this.setState({hasClicked: true});
-      };
+
+
+    createLobby(){
+        console.log("Attempting to create room");
+        //Change this later to read the lobby name of a certain clicked lobby
+        const lobby_name = "1";
+        //Get Username, vest_id, gun_id, location if user is creating lobby
+        const URL = "https://us-central1-lasertag-battle-royale.cloudfunctions.net/initalizeGame"
+        let form_body = [];
+      
+        let userID = "userID" + "=" + "Jasmine"; //Replace Name with actual userID
+        let laserGunID = "laserGunID" + "=" + "1"; //Replace Number with entered laserGunID
+        let vestID = "vestID" + "=" + "1"; //Replace Number with entered vestID
+        let latitude = "latitude" + "=" + "-33"; //Replace with queried lat & long
+        let longitude = "longitude" + "=" + "33";
+        let radius = "radius" + "=" + this.state.radius;
+        form_body.push(userID);
+        form_body.push(laserGunID);
+        form_body.push(vestID);
+        form_body.push(latitude);
+        form_body.push(longitude);
+        form_body.push(radius);
+        form_body = form_body.join('&');
+
+        let other_params = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+            },
+            body: form_body
+        };
+
+        return fetch(URL, other_params)
+            .then(res => {
+              if (res.ok){
+                let copy = res.clone();
+                return copy.text();
+                }
+              }  
+            )
+            .then( data => {
+              return data;
+            })
+            .catch(error =>{
+                console.log(error);
+            });
+      }
+
+      createLobbyEvaluated = () => {
+        let _this = this;
+        let prom = this.createLobby();
+        prom.then((response) =>{
+          console.log("RES: ", response);
+          return response;
+        }).then((data) =>{
+          console.log(data);
+        }).catch((err) =>{
+          console.log(err);
+        });
+      }
+    
+      setRoomID = () => {
+        this.createLobbyEvaluated.bind(this);
+        let rID = this.createLobbyEvaluated();
+        this.setState({roomid: rID});
+        console.log("RID: ", this.state.roomid);
+      }
 
     render(){
       if (this.state.hasClicked === false) {
@@ -56,12 +121,15 @@ class CreateRoom extends React.Component {
    ));
         return (
             <div>
+               <Button variant="outlined" component={Link} to="/lobby">Back</Button>
                 <h1>Create Lobby</h1>
                 <TextField id="lobby-name" label="Lobby Name"></TextField>
                 <form autoComplete="off">
                     <FormControl>
-                        <Select id = "num-players" value={this.num_players}
-                            onChange={this.handleChange}>
+
+                        <Select id = "num-players"
+                            onChange={this.handleChange}
+                            value = {this.state.num_players}>
                             <MenuItem value="">
                                 <em>None</em>
                             </MenuItem>
@@ -82,7 +150,7 @@ class CreateRoom extends React.Component {
         </GoogleMapExample>
         <FormControl>
                   <Select
-            value={this.state.age}
+            value={this.state.radius}
             onChange={this.handleRadiusChange}
           >
         
@@ -95,14 +163,14 @@ class CreateRoom extends React.Component {
             <MenuItem value={1700}>3000</MenuItem>
           </Select>
         </FormControl>
-        
-                <Button className="create-room-button" variant="outlined"  onClick={this.createLobby}>Create Lobby</Button>
-                <Button variant="outlined" component={Link} to="/lobby">Back</Button>
+
+                <Button className="create-room-button" variant="outlined" component={Link} to="/lobby/waiting" onClick={this.setRoomID}>Create Lobby</Button>
+               
             </div>
         );
       } else {
         return(
-        <WaitingRoom latitude={this.state.lat} longitude={this.state.lng} radius={this.state.radius}/>
+        <WaitingRoom latitude={this.state.lat} longitude={this.state.lng} radius={this.state.radius} roomid={this.state.roomid}/>
         );
       }
     }
