a958106439e1d8747106951bb1cc924af9403b8a
Jasmine Kwong <jckwong0703@gmail.com>
2019-05-30 12:18:44 -0700

    Added request to grab user info

--- laser-tag-battle-royale-app/src/src/lobby.js
+++ laser-tag-battle-royale-app/src/src/lobby.js
@@ -27,7 +27,53 @@ class Lobby extends React.Component {
   state = {
     username: this.props.username //Note for jasmine: This now holds the username
   }
+
+  getUserInfoInit = () => {
+    let form_body = [];
+    let URL = "https://us-central1-lasertag-battle-royale.cloudfunctions.net/playerInfo";
+    let name = "name" + "=" + this.props.username;
+    form_body.push(name);
+    let other_params = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+      },
+      body: form_body
+    };
+
+    return fetch(URL, other_params)
+    .then(res => {
+      if (res.ok){
+        let copy = res.clone();
+        return copy.json();
+        }
+      }  
+    )
+    .then( data => {
+      console.log(data);
+      return data;
+    })
+            .catch(error =>{
+                console.log(error);
+            });
+  };
+
+  getUserInfo = () => {
+    let prom = this.getUserInfoInit();
+
+    prom.then(res => {
+      console.log("Res2: " + res);
+    }) .then ( data => {
+      console.log("Data2: " + data);
+    }). catch ( error => {
+      console.log(error);
+    })
+  };
+
+
   render() {
+    let player_info = this.getUserInfo();
+    console.log(player_info);
     return (
       <div className = "root">   
         <Button className="Button" variant="outlined" component = {Link} to="/">Back</Button>
